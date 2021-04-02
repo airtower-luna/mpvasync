785672f15c2ed949a0148793d98b0fdfe119848e
Fiona Klute <fiona.klute@gmx.de>
2021-04-02 20:46:18 +0200

    Autocomplete mpv properties

--- mpvasync.py
+++ mpvasync.py
@@ -4,6 +4,8 @@ import asyncio
 import json
 import logging
 import os.path
+import re
+import subprocess
 import sys
 from contextlib import asynccontextmanager
 from typing import Any, Dict, AsyncIterable, Mapping, Optional, Sequence, Set
@@ -206,12 +208,19 @@ if __name__ == '__main__':
         help='monitor this property (may be specified multiple times)')
     pget = subparsers.add_parser('get-property', help='read mpv properties')
     pget.set_defaults(func=get_property)
-    pget.add_argument(
+    pselect = pget.add_argument(
         'properties', nargs='+', metavar='PROPERTY', help='property to read')
 
     # enable bash completion if argcomplete is available
     try:
         import argcomplete
+        from argcomplete.completers import ChoicesCompleter
+        lproc = subprocess.run(
+            ['mpv', '--input-ipc-server=', '--list-properties'],
+            stdout=subprocess.PIPE, text=True)
+        pselect.completer = ChoicesCompleter(
+            m.group(1) for m in re.finditer(
+                r'^\s([-\w]+)$', lproc.stdout, re.MULTILINE))
         argcomplete.autocomplete(parser)
     except ImportError:
         pass
