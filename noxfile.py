aff3e6f44939abaa7604c92c9caffa1e5440ffa5
Fiona Klute <fiona.klute@gmx.de>
2024-03-22 21:44:24 +0100

    Additionally test with Python 3.12

--- noxfile.py
+++ noxfile.py
@@ -15,7 +15,7 @@ def typecheck(session):
     session.run('mypy', '.')
 
 
-@nox.session(python=['3.11'])
+@nox.session(python=['3.11', '3.12'])
 def test(session):
     """Run tests, report coverage."""
     session.install('.[tests]')
